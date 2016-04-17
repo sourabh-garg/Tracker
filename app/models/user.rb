8f366a3238b749a6ffa1d0a9702656abb1c63788
Rails study <souabhgrg713@gmail.com>
2016-04-17 22:27:16 +0530

    sdf

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ActiveRecord::Base
    has_many :stocks, through: :user_stocks
 
 
+   def full_name 
+   	return "#{first_name} #{last_name}".strip if (first_name || last_name)
+   	"Anonymous"
+   end
+ 
    def can_add_stocks?(ticker_symbol)
 
    	under_stock_limit? && !stock_already_added?(ticker_symbol)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417102110) do
+ActiveRecord::Schema.define(version: 20160417133233) do
 
   create_table "stocks", force: :cascade do |t|
     t.string   "ticker"
@@ -41,10 +41,18 @@ ActiveRecord::Schema.define(version: 20160417102110) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
+  create_table "userstocks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "stock_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
